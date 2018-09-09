4d0db58a6aee2abe269524ca2f91960aaec71093
Justin Tan <justin@driver.xyz>
2018-09-09 12:59:43 +0800

    Use http status code as error code

---
+++ errors/error.go
@@ -0,0 +1,69 @@
+package errors
+
+import (
+	"github.com/gopub/types/errors/internal"
+	"net/http"
+)
+
+type Error interface {
+	error
+	Code() int
+	Message() string
+}
+
+type FieldError interface {
+	Error
+	Field() string
+}
+
+func NewError(code int, msg string) Error {
+	return internal.NewError(code, msg)
+}
+
+func NewFieldError(code int, msg, field string) Error {
+	return internal.NewFieldError(code, msg, field)
+}
+
+func InternalError(message string) Error {
+	return NewError(http.StatusInternalServerError, message)
+}
+
+func BadRequest(message string) Error {
+	return NewError(http.StatusBadRequest, message)
+}
+
+func BadRequestField(field string) Error {
+	return NewFieldError(http.StatusBadRequest, http.StatusText(http.StatusBadRequest), field)
+}
+
+func Unauthorized(message string) Error {
+	return NewError(http.StatusUnauthorized, message)
+}
+
+func UnauthorizedField(field string) Error {
+	return NewFieldError(http.StatusUnauthorized, http.StatusText(http.StatusUnauthorized), field)
+}
+
+func Forbidden(message string) Error {
+	return NewError(http.StatusForbidden, message)
+}
+
+func ForbiddenField(field string) Error {
+	return NewFieldError(http.StatusForbidden, http.StatusText(http.StatusForbidden), field)
+}
+
+func NotFound(message string) Error {
+	return NewError(http.StatusNotFound, message)
+}
+
+func NotFoundField(field string) Error {
+	return NewFieldError(http.StatusNotFound, http.StatusText(http.StatusNotFound), field)
+}
+
+func Conflict(message string) Error {
+	return NewError(http.StatusConflict, message)
+}
+
+func ConflictField(field string) Error {
+	return NewFieldError(http.StatusConflict, http.StatusText(http.StatusConflict), field)
+}
