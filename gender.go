527f01bf18971108cb013cf6fb31f9670bf95d77
JustinTan <mrnatande@gmail.com>
2018-10-24 10:58:26 +0800

    Valid gender

--- gender.go
+++ gender.go
@@ -6,3 +6,14 @@ const (
 	Male   Gender = "male"
 	Female Gender = "female"
 )
+
+func (g Gender) IsValid() bool {
+	switch g {
+	case Male:
+		return true
+	case Female:
+		return true
+	default:
+		return false
+	}
+}
