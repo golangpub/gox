8e5b3da23b454283bff1e3f53e08dcf450ca6e7c
JustinTan <mrnatande@gmail.com>
2018-10-30 16:21:13 +0800

    Remove RegisterAnys

--- init.go
+++ init.go
@@ -1,10 +1,5 @@
 package types
 
 func init() {
-	RegisterAny(&Image{})
-	RegisterAny(&Audio{})
-	RegisterAny(&Video{})
-	RegisterAny(&File{})
-	RegisterAny(&WebPage{})
-	RegisterAny(&RichText{})
+
 }
