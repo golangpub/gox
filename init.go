1d9f8740e45c2195587cecc304e725dd993f10a6
JustinTan <mrnatande@gmail.com>
2018-11-26 23:20:16 +0800

    Register types

--- init.go
+++ init.go
@@ -1,5 +1,10 @@
 package types
 
 func init() {
-
+	RegisterAny(&Image{})
+	RegisterAny(&Video{})
+	RegisterAny(&Audio{})
+	RegisterAny(&WebPage{})
+	RegisterAny(&File{})
+	RegisterAny(&RichText{})
 }
