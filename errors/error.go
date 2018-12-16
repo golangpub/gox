10dc73d30d33ec9baf3f6b5d0548fca470dd3de2
JustinTan <mrnatande@gmail.com>
2018-12-16 17:14:41 +0800

    Add SubError

--- errors/error.go
+++ errors/error.go
@@ -3,7 +3,6 @@ package errors
 import (
 	"github.com/gopub/types/errors/internal"
 	"net/http"
-	"strings"
 )
 
 type Error interface {
@@ -12,17 +11,8 @@ type Error interface {
 	Message() string
 }
 
-type FieldError interface {
-	Error
-	Field() string
-}
-
 func NewError(code int, msg string) Error {
 	return internal.NewError(code, msg)
-}
-
-func NewFieldError(code int, msg, field string) Error {
-	return internal.NewFieldError(code, msg, field)
 }
 
 func InternalError(message string) Error {
@@ -33,38 +23,18 @@ func BadRequest(message string) Error {
 	return NewError(http.StatusBadRequest, message)
 }
 
-func BadRequestField(field string) Error {
-	return NewFieldError(http.StatusBadRequest, strings.ToLower(http.StatusText(http.StatusBadRequest)), field)
-}
-
 func Unauthorized(message string) Error {
 	return NewError(http.StatusUnauthorized, message)
-}
-
-func UnauthorizedField(field string) Error {
-	return NewFieldError(http.StatusUnauthorized, strings.ToLower(http.StatusText(http.StatusUnauthorized)), field)
 }
 
 func Forbidden(message string) Error {
 	return NewError(http.StatusForbidden, message)
 }
 
-func ForbiddenField(field string) Error {
-	return NewFieldError(http.StatusForbidden, strings.ToLower(http.StatusText(http.StatusForbidden)), field)
-}
-
 func NotFound(message string) Error {
 	return NewError(http.StatusNotFound, message)
 }
 
-func NotFoundField(field string) Error {
-	return NewFieldError(http.StatusNotFound, strings.ToLower(http.StatusText(http.StatusNotFound)), field)
-}
-
 func Conflict(message string) Error {
 	return NewError(http.StatusConflict, message)
-}
-
-func ConflictField(field string) Error {
-	return NewFieldError(http.StatusConflict, strings.ToLower(http.StatusText(http.StatusConflict)), field)
 }
---
+++ errors/sub_error.go
@@ -0,0 +1,39 @@
+package errors
+
+import (
+	"github.com/gopub/types/errors/internal"
+	"net/http"
+)
+
+type SubError interface {
+	Error
+	SubCode() int
+}
+
+func NewSubError(code, subCode int, msg string) Error {
+	return internal.NewSubError(code, subCode, msg)
+}
+
+func InternalSubError(subCode int, message string) Error {
+	return NewSubError(http.StatusInternalServerError, subCode, message)
+}
+
+func BadRequestSub(subCode int, message string) Error {
+	return NewSubError(http.StatusBadRequest, subCode, message)
+}
+
+func UnauthorizedSub(subCode int, message string) Error {
+	return NewSubError(http.StatusUnauthorized, subCode, message)
+}
+
+func ForbiddenSub(subCode int, message string) Error {
+	return NewSubError(http.StatusForbidden, subCode, message)
+}
+
+func NotFoundFieldSub(subCode int, message string) Error {
+	return NewSubError(http.StatusNotFound, subCode, message)
+}
+
+func ConflictSub(subCode int, message string) Error {
+	return NewSubError(http.StatusConflict, subCode, message)
+}
