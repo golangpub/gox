6549cdbfe648c8e9a94f1da64a8b18ad5f433493
JustinTan <mrnatande@gmail.com>
2018-12-18 22:29:17 +0800

    update

--- phone_number.go
+++ phone_number.go
@@ -5,7 +5,7 @@ import "fmt"
 type PhoneNumber struct {
 	CountryCode    int    `json:"country_code"`
 	NationalNumber int64  `json:"national_number"`
-	Extension      string `json:"extension,omitempty"`
+	Extension      string `json:"extension,omitempty" sql:"type:VARCHAR(10)"`
 }
 
 func (n *PhoneNumber) String() string {
