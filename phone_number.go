4595ff1621a01badba8ca7c475aae885da174f07
JustinTan <mrnatande@gmail.com>
2018-12-18 16:28:28 +0800

    update

--- phone_number.go
+++ phone_number.go
@@ -3,10 +3,10 @@ package types
 import "fmt"
 
 type PhoneNumber struct {
-	CountryCode string `json:"country_code"`
+	CountryCode int    `json:"country_code"`
 	Number      string `json:"number"`
 }
 
 func (n *PhoneNumber) String() string {
-	return fmt.Sprintf("+%s%s", n.CountryCode, n.Number)
+	return fmt.Sprintf("+%d%s", n.CountryCode, n.Number)
 }
