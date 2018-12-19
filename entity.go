dfb997b2ac84f160e4a9190bf9a38605c7692a52
JustinTan <mrnatande@gmail.com>
2018-12-19 17:45:49 +0800

    Add BigInt

--- entity.go
+++ entity.go
@@ -1,9 +1,50 @@
 package types
 
-import "time"
+import (
+	"database/sql/driver"
+	"errors"
+	"fmt"
+	"math/big"
+	"time"
+)
 
 type BaseEntity struct {
 	ID        ID        `json:"id"`
 	CreatedAt time.Time `json:"created_at"`
 	UpdatedAt time.Time `json:"updated_at"`
+}
+
+type BigInt big.Int
+
+func (i *BigInt) Scan(src interface{}) error {
+	if src == nil {
+		return nil
+	}
+
+	var s string
+	var ok bool
+	s, ok = src.(string)
+	if !ok {
+		b, ok := src.([]byte)
+		if ok {
+			s = string(b)
+		}
+	}
+
+	if !ok {
+		return errors.New(fmt.Sprintf("failed to parse %v into big.Int", src))
+	}
+
+	_, ok = (*big.Int)(i).SetString(s, 10)
+	if !ok {
+		return errors.New(fmt.Sprintf("failed to parse %v into big.Int", src))
+	}
+	return nil
+}
+
+func (i *BigInt) Value() (driver.Value, error) {
+	if i == nil {
+		return nil, nil
+	}
+	return (*big.Int)(i).String(), nil
 }
