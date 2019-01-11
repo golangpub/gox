8574460561f610254aa8c2dbc14678b83ad9111e
JustinTan <mrnatande@gmail.com>
2019-01-11 11:20:54 +0800

    Rollback

--- entity.go
+++ entity.go
@@ -3,40 +3,15 @@ package types
 import (
 	"database/sql"
 	"database/sql/driver"
-	"encoding/json"
 	"errors"
 	"fmt"
 	"math/big"
 	"time"
 )
 
-type jsonEntityTime struct {
-	CreatedAt int64 `json:"created_at"`
-	UpdatedAt int64 `json:"updated_at"`
-}
-
 type EntityTime struct {
 	CreatedAt time.Time `json:"created_at"`
 	UpdatedAt time.Time `json:"updated_at"`
-}
-
-func (e *EntityTime) MarshalJSON() ([]byte, error) {
-	v := &jsonEntityTime{
-		CreatedAt: e.CreatedAt.Unix(),
-		UpdatedAt: e.UpdatedAt.Unix(),
-	}
-	return json.Marshal(v)
-}
-
-func (e *EntityTime) UnmarshalJSON(data []byte) error {
-	var v *jsonEntityTime
-	err := json.Unmarshal(data, &v)
-	if err != nil {
-		return err
-	}
-	e.CreatedAt = time.Unix(v.CreatedAt, 0)
-	e.UpdatedAt = time.Unix(v.UpdatedAt, 0)
-	return nil
 }
 
 type BaseEntity struct {
