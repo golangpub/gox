b108d7f8531dfb97f05d73cec1250b597f8cddaf
JustinTan <mrnatande@gmail.com>
2019-02-23 20:12:08 +0800

    update

---
+++ string.go
@@ -0,0 +1,27 @@
+package gox
+
+import (
+	"regexp"
+	"strings"
+)
+
+func SmartLen(s string) int {
+	n := 0
+	for _, c := range s {
+		if c <= 255 {
+			n += 1
+		} else {
+			n += 2
+		}
+	}
+
+	return n
+}
+
+var whitespaceRegexp = regexp.MustCompile("\\s")
+
+func CombineSpaces(s string) string {
+	s = strings.TrimSpace(s)
+	s = whitespaceRegexp.ReplaceAllString(s, " ")
+	return s
+}
