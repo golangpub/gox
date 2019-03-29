7cff8b31751d94d5b6afea73c81e69287ea48628
JustinTan <mrnatande@gmail.com>
2019-03-29 14:40:29 +0800

    update

---
+++ clock.go
@@ -0,0 +1,18 @@
+package gox
+
+import "time"
+
+type Clock interface {
+	Now() time.Time
+}
+
+type localClock struct {
+}
+
+func (c *localClock) Now() time.Time {
+	return time.Now()
+}
+
+func LocalClock() Clock {
+	return new(localClock)
+}
