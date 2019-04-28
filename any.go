f34a6b6889164cc30beacbaedad6a02786d41d40
JustinTan <mrnatande@gmail.com>
2019-04-28 22:41:02 +0800

    update

--- any.go
+++ any.go
@@ -435,3 +435,14 @@ type WebPage struct {
 func NewWebPage() *WebPage {
 	return new(WebPage)
 }
+
+type Text string
+
+func (t *Text) MarshalJSON() ([]byte, error) {
+	return []byte(*t), nil
+}
+
+func (t *Text) Unmarshal(data []byte) error {
+	*t = Text(string(data))
+	return nil
+}
