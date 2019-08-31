188e61585a2524a8e8b73a786a1335f7be5486a6
JustinTan <mrnatande@gmail.com>
2019-08-31 13:09:50 +0800

    update

--- pattern.go
+++ pattern.go
@@ -13,19 +13,31 @@ var nameRegexp = regexp.MustCompile(`^[a-zA-Z][a-zA-Z0-9\-._]*$`)
 var emailRegexp = regexp.MustCompile("^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$")
 
 func IsEmail(email string) bool {
+	if len(email) == 0 {
+		return false
+	}
 	return emailRegexp.MatchString(email)
 }
 
 func IsURL(s string) bool {
+	if len(s) == 0 {
+		return false
+	}
 	_, err := url.Parse(s)
 	return err == nil
 }
 
 func IsName(name string) bool {
+	if len(name) == 0 {
+		return false
+	}
 	return nameRegexp.MatchString(name)
 }
 
 func IsPhoneNumber(phoneNumber string) bool {
+	if len(phoneNumber) == 0 {
+		return false
+	}
 	parsedNumber, err := phonenumbers.Parse(phoneNumber, "")
 	if err != nil {
 		log.Error(err)
