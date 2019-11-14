9e293e827eda1c706bce5b460a1760c46f259320
JustinTan <mrnatande@gmail.com>
2019-11-14 17:28:19 +0800

    Add type alias

--- sql/sql.go
+++ sql/sql.go
@@ -5,6 +5,29 @@ import (
 	"github.com/gopub/log"
 )
 
+type (
+	DB          = sql.DB
+	Tx          = sql.Tx
+	TxOptions   = sql.TxOptions
+	Stmt        = sql.Stmt
+	Row         = sql.Row
+	Rows        = sql.Rows
+	Conn        = sql.Conn
+	Result      = sql.Result
+	NullInt64   = sql.NullInt64
+	NullTime    = sql.NullTime
+	NullBool    = sql.NullBool
+	NullFloat64 = sql.NullFloat64
+	NullInt32   = sql.NullInt32
+	NullString  = sql.NullString
+)
+
+var (
+	ErrNoRows   = sql.ErrNoRows
+	ErrTxDone   = sql.ErrTxDone
+	ErrConnDone = sql.ErrConnDone
+)
+
 type ColumnScanner interface {
 	Scan(dest ...interface{}) error
 }
