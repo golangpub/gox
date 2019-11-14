3c250998a40bee43d7b24637b510268abe04370e
JustinTan <mrnatande@gmail.com>
2019-11-14 11:36:27 +0800

    Logging

--- env/env.go
+++ env/env.go
@@ -1,6 +1,7 @@
 package env
 
 import (
+	"github.com/gopub/log"
 	"github.com/spf13/viper"
 	"time"
 )
@@ -11,35 +12,49 @@ func init() {
 
 func String(key string, defaultVal string) string {
 	viper.SetDefault(key, defaultVal)
-	return viper.GetString(key)
+	v := viper.GetString(key)
+	log.Debugf("Env: %s=%s", key, v)
+	return v
 }
 
 func Int(key string, defaultVal int) int {
 	viper.SetDefault(key, defaultVal)
-	return viper.GetInt(key)
+	v := viper.GetInt(key)
+	log.Debugf("Env: %s=%d", key, v)
+	return v
 }
 
 func Int64(key string, defaultVal int64) int64 {
 	viper.SetDefault(key, defaultVal)
-	return viper.GetInt64(key)
+	v := viper.GetInt64(key)
+	log.Debugf("Env: %s=%d", key, v)
+	return v
 }
 
 func Int32(key string, defaultVal int32) int32 {
 	viper.SetDefault(key, defaultVal)
-	return viper.GetInt32(key)
+	v := viper.GetInt32(key)
+	log.Debugf("Env: %s=%d", key, v)
+	return v
 }
 
 func Float64(key string, defaultVal float64) float64 {
 	viper.SetDefault(key, defaultVal)
-	return viper.GetFloat64(key)
+	v := viper.GetFloat64(key)
+	log.Debugf("Env: %s=%f", key, v)
+	return v
 }
 
 func Duration(key string, defaultVal time.Duration) time.Duration {
 	viper.SetDefault(key, defaultVal)
-	return viper.GetDuration(key)
+	v := viper.GetDuration(key)
+	log.Debugf("Env: %s=%v", key, v)
+	return v
 }
 
 func Bool(key string, defaultVal bool) bool {
 	viper.SetDefault(key, defaultVal)
-	return viper.GetBool(key)
+	v := viper.GetBool(key)
+	log.Debugf("Env: %s=%t", key, v)
+	return v
 }
