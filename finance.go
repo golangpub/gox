e0bc310be651d3cbf62c247d293617e84ae4036c
JustinTan <mrnatande@gmail.com>
2019-11-16 12:35:07 +0800

    Restore coin

--- finance.go
+++ finance.go
@@ -3,6 +3,7 @@ package gox
 import (
 	"database/sql/driver"
 	"fmt"
+	"math/big"
 
 	"github.com/gopub/gox/sql"
 )
@@ -51,4 +52,50 @@ func (m *Money) Scan(src interface{}) error {
 
 func (m *Money) Value() (driver.Value, error) {
 	return fmt.Sprintf("(%s,%d)", m.Currency, m.Amount), nil
+}
+
+// Money
+type Coin struct {
+	Currency string  `json:"currency"`
+	Amount   big.Int `json:"amount"`
+}
+
+var _ driver.Valuer = (*Coin)(nil)
+var _ sql.Scanner = (*Coin)(nil)
+
+func (c *Coin) String() string {
+	return fmt.Sprintf("%s %s", c.Currency, c.Amount.String())
+}
+
+func (c *Coin) Scan(src interface{}) error {
+	if src == nil {
+		return nil
+	}
+
+	s, err := ParseString(src)
+	if err != nil {
+		return fmt.Errorf("parse string: %w", err)
+	}
+	if len(s) == 0 {
+		return nil
+	}
+
+	fields, err := sql.ParseCompositeFields(s)
+	if err != nil {
+		return fmt.Errorf("parse composite fields %s: %w", s, err)
+	}
+
+	if len(fields) != 2 {
+		return fmt.Errorf("parse composite fields %s: got %v", s, fields)
+	}
+	c.Currency = fields[0]
+	_, ok := c.Amount.SetString(fields[1], 10)
+	if !ok {
+		return fmt.Errorf("parse amount %s failed", fields[1])
+	}
+	return nil
+}
+
+func (c *Coin) Value() (driver.Value, error) {
+	return fmt.Sprintf("(%s,%s)", c.Currency, c.Amount.String()), nil
 }
