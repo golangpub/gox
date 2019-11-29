06683acf6f8ab83f7b9b2b4f5c84539056b8c8c5
JustinTan <mrnatande@gmail.com>
2019-11-29 14:13:30 +0800

    Reverse

--- slice.go
+++ slice.go
@@ -29,3 +29,15 @@ func IndexOfString(strs []string, s string) int {
 
 	return -1
 }
+
+func ReverseIntSlice(s []int) {
+	for i, j := 0, len(s)-1; i < j; i, j = i+1, j-1 {
+		s[i], s[j] = s[j], s[i]
+	}
+}
+
+func ReverseInt64Slice(s []int64) {
+	for i, j := 0, len(s)-1; i < j; i, j = i+1, j-1 {
+		s[i], s[j] = s[j], s[i]
+	}
+}
