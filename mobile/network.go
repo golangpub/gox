9d97662a46753a50d0eef2adaf10b3f8d2e7ea49
JustinTan <mrnatande@gmail.com>
2020-02-21 13:13:09 +0800

    Add mobile utility

--- mobile/network.go
+++ mobile/network.go
@@ -6,6 +6,18 @@ import (
 	"github.com/gopub/gox"
 )
 
+const (
+	NoNetwork = 0
+	Cellular  = 1
+	WIFI      = 2
+)
+
+const (
+	IDle       = 0
+	Connecting = 1
+	Connected  = 2
+)
+
 func GetIP() (net.IP, error) {
 	return gox.GetIP()
 }
