197512889493b8bfa779652e71e1fc06ca9f59a4
JustinTan <mrnatande@gmail.com>
2020-03-08 17:30:59 +0800

    Parse env

---
+++ mobile/env.go
@@ -0,0 +1,15 @@
+package mobile
+
+import "github.com/gopub/gox/env"
+
+func EnvString(key string) string {
+	return env.String(key, "")
+}
+
+func EnvInt(key string) int {
+	return env.Int(key, 0)
+}
+
+func EnvFloat64(key string) float64 {
+	return env.Float64(key, 0)
+}
