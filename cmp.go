adf70d067117f84d9136fc956e24317a3539170a
JustinTan <mrnatande@gmail.com>
2020-03-12 14:24:26 +0800

    Add func Equal

--- cmp.go
+++ cmp.go
@@ -2,6 +2,7 @@ package gox
 
 import (
 	"encoding/json"
+	"reflect"
 
 	"github.com/google/go-cmp/cmp"
 )
@@ -14,4 +15,8 @@ func DiffJSON(v1, v2 interface{}) string {
 	j1, _ := json.Marshal(v1)
 	j2, _ := json.Marshal(v2)
 	return cmp.Diff(string(j1), string(j2))
+}
+
+func Equal(v1, v2 interface{}) bool {
+	return reflect.TypeOf(v1).Comparable() && reflect.TypeOf(v2).Comparable() && v1 == v2
 }
